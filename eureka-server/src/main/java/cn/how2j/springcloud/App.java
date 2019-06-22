fc7797226f7feec5b474564e3e695201cc91e9e5
felton <wudadongfen@126.com>
2019-06-22 11:12:25 +0800

    注册父子服务

---
+++ eureka-server/src/main/java/cn/how2j/springcloud/App.java
@@ -0,0 +1,14 @@
+package cn.how2j.springcloud;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+
